8a68f744449ea04257adc9aed8231dcbaba1a22a
sahilthakurr24 <tsahil6231@gmail.com>
2026-05-28 15:27:41 +0530

    Add form and formField exports to database schema; update journal with new version tags

---
+++ packages/database/models/formField.ts
@@ -0,0 +1,38 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  text,
+  boolean,
+  numeric,
+  pgEnum,
+  unique
+} from "drizzle-orm/pg-core";
+import { formsTable } from "./form";
+
+export const fieldTypeEnum = pgEnum("field_type_enum", [
+  "TEXT",
+  "NUMBER",
+  "EMAIL",
+  "YES_NO",
+  "PASSWORD",
+]);
+
+export const formFieldsTable = pgTable("form_fields", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  label: varchar("label", { length: 100 }).notNull(),
+  label_key: varchar("label_key", { length: 100 }).notNull(),
+  description: text("description"),
+  placeholder: text("placeholder"),
+  isRequired: boolean("is_required").default(false).notNull(),
+  index: numeric("index", { scale: 2 }).notNull(),
+  type: fieldTypeEnum("type").notNull(),
+  formId : uuid('form_id').references(()=> formsTable.id),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+},(table)=>{
+    return {
+        uniqueFormAndIndex : unique().on(table.formId, table.index)
+    }
+});
